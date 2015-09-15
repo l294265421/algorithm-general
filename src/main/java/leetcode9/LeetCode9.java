0b886de770f4e00e83ba5b6a001371f9bd42f5ab
liyuncong <294265421@qq.com>
2015-09-15 13:25:40 +0800

    leetcode9

---
+++ src/main/java/leetcode9/LeetCode9.java
@@ -0,0 +1,55 @@
+package leetcode9;
+
+public class LeetCode9 {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+			return false;
+		}
+        
+        if (x >= 10) {
+			int palindromeCandidate = reverse(x);
+			if (palindromeCandidate != x) {
+				return false;
+			}
+		}
+        
+        return true;
+    }
+    
+	public int reverse(int x) {
+		//最小负数的补码是10000...，在int范围内没有
+		// 与之匹配的相反数；它的实际相反数超出了int
+		// 的范围，所以算作overflow
+		if (x == Integer.MIN_VALUE) {
+			return 0;
+		}
+		
+		int sign = 1;
+		if (x < 0) {
+			sign = -1;
+			x *= -1;
+		}
+		
+		String resultStr = "";
+		while (x >= 10) {
+			int remainder = x % 10;
+			resultStr += remainder;
+			x = x / 10;
+		}
+		resultStr += x;
+		
+		int result = 0;
+		try {
+			result = Integer.valueOf(resultStr) * sign;
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		LeetCode9 leetCode9 = new LeetCode9();
+		System.out.println(leetCode9.isPalindrome(101));
+	}
+}
