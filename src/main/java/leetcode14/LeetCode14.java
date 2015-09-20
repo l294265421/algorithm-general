5fcb1bdf6c2f292f3354d479ff1a401e8f9296c6
李云聪 <294265421@qq.com>
2015-09-20 13:29:53 +0800

    leetcode14

---
+++ src/main/java/leetcode14/LeetCode14.java
@@ -0,0 +1,29 @@
+package leetcode14;
+
+public class LeetCode14 {
+	public String longestCommonPrefix(String[] strs) {
+		int len = strs.length;
+		if (len == 0) {
+			return "";
+		}
+		
+		String prefix = strs[0];
+		for(int i = 1; i < len; i++) {
+			String temp = strs[i];
+			while (prefix.length() > 0) {
+				if (temp.startsWith(prefix)) {
+					break;
+				} else {
+					prefix = prefix.substring(0, prefix.length() - 1);
+				}
+			}
+		}
+		return prefix;
+	}
+	
+	public static void main(String[] args) {
+		LeetCode14 leetCode14 = new LeetCode14();
+		String[] test = new String[] {"ddd", "adc", "adf"};
+		System.out.println(leetCode14.longestCommonPrefix(test));
+	}
+}
