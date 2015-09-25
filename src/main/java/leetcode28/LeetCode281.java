6919b75816837733d016f02d183c3d4131a6bbe0
李云聪 <294265421@qq.com>
2015-09-25 20:43:14 +0800

    leetcod28

---
+++ src/main/java/leetcode28/LeetCode281.java
@@ -0,0 +1,12 @@
+package leetcode28;
+
+public class LeetCode281 {
+	public int strStr(String haystack, String needle) {
+		return haystack.indexOf(needle);
+	}
+	
+	public static void main(String[] args) {
+		LeetCode281 leetCode28 = new LeetCode281();
+		System.out.println(leetCode28.strStr("liyuncong", "yund"));
+	}
+}
