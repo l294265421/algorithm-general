d88c7d4a2a27cf088c805d46e1c04db7123a4643
李云聪 <294265421@qq.com>
2015-09-27 11:34:45 +0800

    leetcode35

---
+++ src/main/java/leetcode29/LeetCode291.java
@@ -0,0 +1,46 @@
+package leetcode29;
+
+public class LeetCode291 {
+	public int divide(int dividend, int divisor) {
+		if (divisor == 0) {
+			return Integer.MAX_VALUE;
+		}
+		
+		if(dividend == Integer.MIN_VALUE && divisor == -1) {
+			return Integer.MAX_VALUE;
+		}
+		
+		int result = 0;
+		if (dividend >= 0 && divisor > 0) {
+			dividend -= divisor;
+			while (dividend >= 0) {
+				result++;
+				dividend -= divisor;
+			}
+		} else if (dividend < 0 && divisor < 0) {
+			dividend -= divisor;
+			while (dividend <= 0) {
+				result++;
+				dividend -= divisor;
+			}
+		} else if (dividend >= 0 && divisor < 0) {
+			dividend += divisor;
+			while (dividend >= 0) {
+				result--;
+				dividend += divisor;
+			}
+		} else {
+			dividend += divisor;
+			while (dividend<= 0) {
+				result--;
+				dividend += divisor;
+			}
+		}
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		LeetCode291 leetCode29 = new LeetCode291();
+		System.out.println(leetCode29.divide(13, -54));
+	}
+}
