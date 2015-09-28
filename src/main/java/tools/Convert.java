1071a09a1ef9048b73c93a263793d93a35e14f81
liyuncong <294265421@qq.com>
2015-09-28 15:10:12 +0800

    删除了一些文件

--- src/main/java/tools/Convert.java
+++
@@ -1,5 +0,0 @@
-package tools;
-
-public class Convert {
-
-}
--- src/main/java/tools/DefaultValue.java
+++
@@ -1,8 +0,0 @@
-package tools;
-
-import java.nio.charset.Charset;
-
-public class DefaultValue {
-	public static final Charset FROMCHARSET = Charset.forName("UTF-8");
-	public static final Charset TOCHARSET = Charset.forName("UTF-8");
-}
