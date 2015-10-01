5283a2fbf73601ef1f39cd3d8f6de7b12cd1f9d0
李云聪 <294265421@qq.com>
2015-10-01 16:26:11 +0800

    leetcode60-61

---
+++ src/main/java/leetcode61/ListNode.java
@@ -0,0 +1,16 @@
+package leetcode61;
+
+public class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode(int x) {
+		val = x;
+	}
+
+	@Override
+	public String toString() {
+		return "ListNode [val=" + val + ", next=" + next + "]";
+	}
+	
+}
