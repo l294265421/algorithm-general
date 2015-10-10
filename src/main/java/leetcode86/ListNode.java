1bf6e293fc705cfd0170452f06ee7b725c36813f
liyuncong <294265421@qq.com>
2015-10-10 10:19:42 +0800

    leetcode86

---
+++ src/main/java/leetcode86/ListNode.java
@@ -0,0 +1,17 @@
+package leetcode86;
+
+public class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode(int x) {
+		val = x;
+	}
+
+	@Override
+	public String toString() {
+		return "ListNode [val=" + val + ", next=" + next + "]";
+	}
+	
+	
+}
