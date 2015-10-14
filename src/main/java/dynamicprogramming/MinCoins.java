51b75af154d202e3a3686c2ed635371608fce0fa
liyuncong <294265421@qq.com>
2015-10-14 16:14:20 +0800

    leetcode91

--- src/main/java/dynamicprogramming/MinCoins.java
+++ src/main/java/dynamicprogramming/MinCoins.java
@@ -1,21 +1,15 @@
 package dynamicprogramming;
 
 public class MinCoins {
-	 static void main(String[] args) {
+	public static void main(String[] args) {
 		int[] coins = { 1, 3, 5 };
 		int value = 11;
-		int[] solu = new int[value];
-		int min = new MinCoins().solution(coins, value, solu);
-		for (int i = value - 1; i >= 0;) {
-			System.out.print(solu[i] + "->");
-			i = i - solu[i];
-		}
-		System.out.println();
+		int min = new MinCoins().solution(coins, value);
 		System.out.println(min);
 
 	}
 
-	private int solution(int[] coins, int value, int[] solu) {
+	private int solution(int[] coins, int value) {
 		int[] mins = new int[value + 1];
 		mins[0] = 0;
 		for (int i = 1; i <= value; i++)
@@ -23,10 +17,8 @@ public class MinCoins {
 		for (int i = 1; i <= value; i++) {
 			for (int j = 0; j < coins.length; j++) {
 				// coins[j]表示硬币的值，i表示要求的值
-				// 不断优化solu[i - 1]
 				if (coins[j] <= i && mins[i] > mins[i - coins[j]] + 1) {
 					mins[i] = mins[i - coins[j]] + 1;
-					solu[i - 1] = coins[j];
 				}
 			}
 		}
--- src/main/java/leetcode91/LeetCode91.java
+++ src/main/java/leetcode91/LeetCode91.java
@@ -2,32 +2,54 @@ package leetcode91;
 
 public class LeetCode91 {
 	private int ways;
+
 	public int numDecodings(String s) {
+		if (s.length() == 0) {
+			return 0;
+		}
 		trial(s, 0);
 		return this.ways;
 	}
-	
+
 	public void trial(String s, int index) {
-		if (index == s.length()) {
+		int slen = s.length();
+		if (index == slen) {
 			// 刚好达到s.length()才刚好解码
 			this.ways++;
 			return;
-		} else if (index > s.length()) {
-			return;
 		}
-		
-		if (s.charAt(index) == '1' || s.charAt(index) == '2') {
-			// 把index解码为一个字符
-			trial(s, index + 1);
-			// 把index和index+1一起解释为一个字符
-			trial(s, index + 2);
+
+		// 必须保证编码的第一个数字不为0
+		if (s.charAt(index) == '0') {
+			System.out.println(s.charAt(index));
+			return;
+		} else if (s.charAt(index) == '1' || s.charAt(index) == '2') {
+			if (slen == index + 1) {
+				// 把index解码为一个字符
+				trial(s, index + 1);
+			} else {
+				if (s.charAt(index + 1) == '0') {
+					// 把index和index+1一起解释为一个字符
+					trial(s, index + 2);
+				} else {
+					// 把index解码为一个字符
+					trial(s, index + 1);
+					// 把index和index+1一起解释为一个字符
+					int val = Integer.parseInt(
+							s.substring(index, index + 2));
+					if (val <= 26) {
+						trial(s, index + 2);
+					}
+				}
+			}
 		} else {
+			// 把index解码为一个字符
 			trial(s, index + 1);
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		LeetCode91 leetCode91 = new LeetCode91();
-		System.out.println(leetCode91.numDecodings("122"));
+		System.out.println(leetCode91.numDecodings("9317949759856497357254398763219839323723136763131916377913495416692666785978758414629119614215967159"));
 	}
 }
