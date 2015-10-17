ced3477ee71ee097eb1cad92897ee0916fd4ff9e
李云聪 <294265421@qq.com>
2015-10-17 23:07:40 +0800

    leetcode93 94

---
+++ src/main/java/leetcode94/LeetCode941.java
@@ -0,0 +1,30 @@
+package leetcode94;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeetCode941 {
+	public List<Integer> inorderTraversal(TreeNode root) {
+		List<Integer> resultList = new ArrayList<Integer>();
+		inorderTraversal(root, resultList);
+		return resultList;
+	}
+
+	public void inorderTraversal(TreeNode root, List<Integer> resultList) {
+		if (root != null) {
+			inorderTraversal(root.left, resultList);
+			resultList.add(root.val);
+			inorderTraversal(root.right, resultList);
+		}
+	}
+	
+	public static void main(String[] args) {
+		LeetCode941 leetCode94 = new LeetCode941();
+		TreeNode one = new TreeNode(1);
+		TreeNode two = new TreeNode(2);
+		one.right = two;
+		TreeNode three = new TreeNode(3);
+		two.left = three;
+		System.out.println(leetCode94.inorderTraversal(one));
+	}
+}
