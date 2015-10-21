b7b887053569c7605245fd5d0d2871b60a9a3d38
liyuncong <294265421@qq.com>
2015-10-21 11:03:34 +0800

    leetcode98

--- src/main/java/leetcode98/LeetCode98.java
+++ src/main/java/leetcode98/LeetCode98.java
@@ -1,40 +1,41 @@
 package leetcode98;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LeetCode98 {
 	public boolean isValidBST(TreeNode root) {
 		if (root == null) {
 			return true;
 		}
-		TreeNode leftNode = root.left;
-		boolean left = false;
-		if (leftNode == null) {
-			left = true;
-		} else {
-			if (leftNode.val >= root.val) {
-				left = false;
-			} else {
-				left = isValidBST(leftNode);
+		
+		List<TreeNode> treeNodeList = new ArrayList<TreeNode>();
+		inorder(root, treeNodeList);
+		int size = treeNodeList.size();
+		for(int i = 1; i < size; i++) {
+			if (treeNodeList.get(i - 1).val >= treeNodeList.get(i).val) {
+				return false;
 			}
 		}
-		
-		TreeNode rightNode = root.right;
-		boolean right = false;
-		if (rightNode == null) {
-			right = true;
-		} else {
-			if (rightNode.val <= root.val) {
-				right = false;
-			} else {
-				right = isValidBST(rightNode);
-			}
+		return true;
+	}
+	
+	/**
+	 * 获得二叉搜索树的线性表示，即有序链表
+	 * @param root
+	 * @param treeNodeList
+	 */
+	public void inorder(TreeNode root, List<TreeNode> treeNodeList) {
+		if (root != null) {
+			inorder(root.left, treeNodeList);
+			treeNodeList.add(root);
+			inorder(root.right, treeNodeList);
 		}
-		
-		return left  && right;
 	}
 	
 	public static void main(String[] args) {
-		TreeNode node1 = new TreeNode(2);
-		TreeNode node2 = new TreeNode(1);
+		TreeNode node1 = new TreeNode(1);
+		TreeNode node2 = new TreeNode(2);
 		TreeNode node3 = new TreeNode(3);
 		node1.left = node2;
 		node1.right = node3;
