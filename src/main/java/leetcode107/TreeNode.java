aa12d9b6ff2b0129093bcd71818b7fd3576ef28a
李云聪 <294265421@qq.com>
2015-10-24 12:53:35 +0800

    leetcode107

---
+++ src/main/java/leetcode107/TreeNode.java
@@ -0,0 +1,11 @@
+package leetcode107;
+
+/**
+ * Created by liyuncong on 10/22/15.
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
