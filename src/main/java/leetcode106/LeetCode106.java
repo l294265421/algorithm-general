e7efc97e48c5fac831164d44521cb76f9408783b
李云聪 <294265421@qq.com>
2015-10-24 12:40:54 +0800

    leetcode106

--- src/main/java/leetcode106/LeetCode106.java
+++ src/main/java/leetcode106/LeetCode106.java
@@ -1,32 +1,37 @@
 package leetcode106;
 
+/**
+ * 核心思想：后根遍历树得到的根元素顺序刚好和先根遍历相反；其它的和105题一样
+ * @author yuncong
+ *
+ */
 public class LeetCode106 {
 	private int rootTh;
-	public TreeNode buildTree(int[] preorder, int[] inorder) {
-		if (preorder == null) {
+	public TreeNode buildTree(int[] inorder, int[] postorder) {
+		if (postorder == null) {
 			return null;
 		}
-		int len = preorder.length;
+		int len = postorder.length;
 		if (len == 0) {
 			return null;
 		}
 		rootTh = len - 1;
 		
-		int numZero = preorder[rootTh];
+		int numZero = postorder[rootTh];
 		this.rootTh--;
 		TreeNode root = new TreeNode(numZero);
 		int rootNumIndexInorder = indexOf(inorder, 0, len - 1, numZero);
-		buildTree(preorder, root, inorder, rootNumIndexInorder + 1, len - 1, 1);
-		buildTree(preorder, root, inorder, 0, rootNumIndexInorder - 1, 0);
+		buildTree(postorder, root, inorder, rootNumIndexInorder + 1, len - 1, 1);
+		buildTree(postorder, root, inorder, 0, rootNumIndexInorder - 1, 0);
 		return root;
 	}
 	
-	public void buildTree(int[] preorder, TreeNode parent, int[] inorder, 
+	public void buildTree(int[] postorder, TreeNode parent, int[] inorder, 
 			int left, int right, int leftOrRight) {
 		if (left > right) {
 			return;
 		}
-		int rootTempNum = preorder[this.rootTh];
+		int rootTempNum = postorder[this.rootTh];
 		this.rootTh--;
 		TreeNode rootTemp = new TreeNode(rootTempNum);
 		if (leftOrRight == 0) {
@@ -35,8 +40,8 @@ public class LeetCode106 {
 			parent.right = rootTemp;
 		}
 		int rootTempNumIndexInorder = indexOf(inorder, left, right, rootTempNum);
-		buildTree(preorder, rootTemp, inorder, rootTempNumIndexInorder + 1, right, 1);
-		buildTree(preorder, rootTemp, inorder, left, rootTempNumIndexInorder - 1, 0);
+		buildTree(postorder, rootTemp, inorder, rootTempNumIndexInorder + 1, right, 1);
+		buildTree(postorder, rootTemp, inorder, left, rootTempNumIndexInorder - 1, 0);
 	}
 	
 	public int indexOf(int[] arr, int left , int right, int num) {
@@ -49,9 +54,9 @@ public class LeetCode106 {
 	}
 	
 	public static void main(String[] args) {
-		int[] preorder = new int[] {1,3,2};
-		int[] inorder = new int[] {3,2,1};
+		int[] inorder = new int[] {1,3,2};
+		int[] postorder = new int[] {3,2,1};
 		LeetCode106 leetCode105 = new LeetCode106();
-		System.out.println(leetCode105.buildTree(preorder, inorder));
+		System.out.println(leetCode105.buildTree(inorder, postorder));
 	}
 }
