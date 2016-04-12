9113c0a66d05edaed330496e94e8e37f85e5e7b0
liyuncong <liyuncong@172.22.21.35>
2016-04-12 18:35:23 +0800

    RSA

---
+++ src/main/java/encrypt/RSA.java
@@ -0,0 +1,92 @@
+package encrypt;
+
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.interfaces.RSAPrivateCrtKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
+import org.apache.commons.codec.binary.Base64;
+
+public class RSA {
+	private static String src = "liyuncong";
+	public static void main(String[] args) {
+		jdkRSA();
+	}
+	public static void jdkRSA() {
+		try {
+			// 1.初始化秘钥
+			KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+			keyPairGenerator.initialize(512);
+			KeyPair keyPair = keyPairGenerator.generateKeyPair();
+			RSAPublicKey rsaPublicKey = (RSAPublicKey) keyPair.getPublic();
+			RSAPrivateCrtKey rsaPrivateCrtKey = (RSAPrivateCrtKey) keyPair.getPrivate();
+			
+			// 2. 私钥加密，公钥解密——加密
+			PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(rsaPrivateCrtKey.getEncoded());
+			KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+			PrivateKey privateKey = keyFactory.generatePrivate(pkcs8EncodedKeySpec);
+			Cipher cipher = Cipher.getInstance("RSA");
+			cipher.init(Cipher.ENCRYPT_MODE, privateKey);
+			byte[] result = cipher.doFinal(src.getBytes());
+			System.out.println(Base64.encodeBase64String(result));
+			
+			// 3.私钥加密、公钥解密——解密
+			X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(rsaPublicKey.getEncoded());
+			keyFactory = KeyFactory.getInstance("RSA");
+			PublicKey publicKey = keyFactory.generatePublic(x509EncodedKeySpec);
+			cipher = Cipher.getInstance("RSA");
+			cipher.init(Cipher.DECRYPT_MODE, publicKey);
+			result = cipher.doFinal(result);
+			System.out.println(new String(result));
+			
+			// 4.公钥加密、私钥解密——加密
+			x509EncodedKeySpec = new X509EncodedKeySpec(rsaPublicKey.getEncoded());
+			keyFactory = KeyFactory.getInstance("RSA");
+			publicKey = keyFactory.generatePublic(x509EncodedKeySpec);
+			cipher = Cipher.getInstance("RSA");
+			cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+			result = cipher.doFinal(src.getBytes());
+			System.out.println(Base64.encodeBase64String(result));
+			
+			// 4.公钥加密、私钥解密——解密
+			pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(rsaPrivateCrtKey.getEncoded());
+			keyFactory = KeyFactory.getInstance("RSA");
+			privateKey = keyFactory.generatePrivate(pkcs8EncodedKeySpec);
+			cipher = Cipher.getInstance("RSA");
+			cipher.init(Cipher.DECRYPT_MODE, privateKey);
+			result = cipher.doFinal(result);
+			System.out.println(new String(result));
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeySpecException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
