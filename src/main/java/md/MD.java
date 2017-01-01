948ccd2c087787b5f0b508802f2a01a767315b31
liyuncong <294265421@qq.com>
2017-01-01 18:20:30 +0800

    修改使用消息摘要算法的实现

--- src/main/java/md/MD.java
+++ src/main/java/md/MD.java
@@ -2,50 +2,33 @@ package md;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.security.Security;
 
 import org.apache.commons.codec.binary.Hex;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 public class MD {
+	public static final String ALGORITHM_NAME_MD5 = "md5";
+	public static final String ALGORITHM_NAME_MD4 = "md4";
+	public static final String ALGORITHM_NAME_MD2 = "md2";
+	public static final String ALGORITHM_NAME_SHA1 = "sha1";
+	
 	private MD() {
 		
 	}
 
-	public static String MD5(String input) {
-		String md5 = "";
+	public static String md(String input, String algorithmName) {
+		String result = "";
 		try {
-			MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+			MessageDigest messageDigest = MessageDigest.getInstance(algorithmName);
 			byte[] md5bytes = messageDigest.digest(input.getBytes());
-			md5 = Hex.encodeHexString(md5bytes);
+			result = Hex.encodeHexString(md5bytes);
 		} catch (NoSuchAlgorithmException e) {
 			e.printStackTrace();
 		}
-		return md5;
+		return result;
 	}
-
-	public static String MD4(String input) {
-		String md4 = "";
-		try {
-			Security.addProvider(new BouncyCastleProvider());
-			MessageDigest messageDigest = MessageDigest.getInstance("MD4");
-			byte[] md4bytes = messageDigest.digest(input.getBytes());
-			md4 = Hex.encodeHexString(md4bytes);
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		}
-		return md4;
-	}
-
-	public static String MD2(String input) {
-		String md2 = "";
-		try {
-			MessageDigest messageDigest = MessageDigest.getInstance("MD2");
-			byte[] md2bytes = messageDigest.digest(input.getBytes());
-			md2 = Hex.encodeHexString(md2bytes);
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		}
-		return md2;
+	
+	public static void main(String[] args) {
+		String name = "liyuncong";
+		System.out.println(MD.md(name, ALGORITHM_NAME_SHA1));
 	}
 }
